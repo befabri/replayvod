55196a2d97e04d647953383213b8c4c622e6a4b7
Benjamin Fabri <contact@bfabri.me>
2023-06-15 17:04:08 +0200

    Add mongodb & Update followList caching in mongodb for retrieval

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,35 @@
 import axios from "axios";
 import { Request, Response } from "express";
+import { connect, getDbInstance } from "../models/db";
+
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+
+import { v4 as uuidv4 } from "uuid";
 
 export const followList = async (req: Request, res: Response) => {
   if (!req.session?.passport?.user) {
     res.status(401).send("Unauthorized");
     return;
   }
+
   const userId = req.session.passport.user.data[0].id;
   const accessToken = req.session.passport.user.accessToken;
+
   if (userId == undefined) {
     res.status(500).send("Error fetching followed streams");
   }
+
   try {
+    const db = await getDbInstance();
+    const collection = db.collection("followedStreams");
+    const fetchLogCollection = db.collection("fetchLog");
+    const fetchLog = await fetchLogCollection.findOne({ userId: userId }, { sort: { fetchedAt: -1 } });
+    if (fetchLog && fetchLog.fetchedAt > new Date(Date.now() - 5 * 60 * 1000)) {
+      const streams = await collection.find({ fetchId: fetchLog.fetchId }).toArray();
+      res.json(streams);
+      return;
+    }
+    const fetchId = uuidv4();
     const response = await axios({
       method: "get",
       url: `https://api.twitch.tv/helix/streams/followed?user_id=${userId}`,
@@ -22,7 +39,21 @@ export const followList = async (req: Request, res: Response) => {
       },
     });
 
-    console.log(response.data);
+    for (const stream of response.data.data) {
+      const data = {
+        ...stream,
+        fetchedAt: new Date(),
+        fetchId,
+      };
+
+      await collection.findOneAndUpdate(
+        { id: stream.id },
+        { $set: data },
+        { upsert: true, returnDocument: "after" }
+      );
+    }
+    await fetchLogCollection.insertOne({ userId: userId, fetchedAt: new Date(), fetchId: fetchId });
+
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching followed streams:", error);
---
+++ src/models/db.ts
@@ -0,0 +1,53 @@
+import { MongoClient, Db } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const {
+  MONGO_USERNAME: USERNAME,
+  MONGO_PASSWORD: PASSWORD,
+  MONGO_IP: DBIP,
+  MONGO_IP_PROD: DBIP_PROD,
+  MONGO_PORT: DBPORT,
+  MONGO_DBNAME: DBNAME,
+  MONGO_COLLECTION: COLLECTION = "defaultCollection",
+  NODE_ENV: ENVIRONMENT = "development",
+} = process.env;
+
+let url = `mongodb+srv://${USERNAME}:${PASSWORD}@${DBIP}/${DBNAME}?tls=true&authSource=admin&retryWrites=true&w=majority`;
+
+if (ENVIRONMENT === "production") {
+  url = `mongodb+srv://${USERNAME}:${PASSWORD}@${DBIP_PROD}/${DBNAME}?tls=true&authSource=admin&retryWrites=true&w=majority`;
+}
+
+let client: MongoClient | null = null;
+let connection: Promise<Db> | undefined;
+
+async function connect(): Promise<Db> {
+  if (!connection) {
+    console.log("Establishing initial connection...");
+    client = new MongoClient(url);
+    await client.connect();
+    connection = Promise.resolve(client.db(DBNAME));
+  }
+  if (client !== null) {
+    return connection;
+  }
+  throw new Error("Failed to establish database connection.");
+}
+
+async function getDbInstance() {
+  const db = await connect();
+  return db;
+}
+
+async function cleanUp(): Promise<void> {
+  if (client !== null) {
+    await client.close();
+  }
+  process.exit(0);
+}
+
+process.on("SIGINT", cleanUp);
+process.on("SIGTERM", cleanUp);
+
+export { connect, getDbInstance };
