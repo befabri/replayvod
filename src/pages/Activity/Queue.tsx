cf77cde93e035359040f8ffdfc844d4b7805d778
Benjamin Fabri <contact@bfabri.me>
2023-06-19 22:21:57 +0200

    Add refresh interval for the queue

--- src/pages/Activity/Queue.tsx
+++ src/pages/Activity/Queue.tsx
@@ -27,32 +27,42 @@ const Queue: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/videos/all", {
-      credentials: "include",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const pendingVideos = data.filter((video: Video) => video.status === "Pending");
-
-        const convertedVideos = pendingVideos.map((video: Video) => {
-          const startDownloadAtDate = new Date(video.start_download_at);
-          const downloadedAtDate = new Date(video.downloaded_at);
-          video.start_download_at = startDownloadAtDate.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
-          video.downloaded_at = downloadedAtDate.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
-          return video;
-        });
-
-        setVideos(convertedVideos);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error(`Error fetching data: ${error}`);
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/videos/all", {
+        credentials: "include",
       });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      const pendingVideos = data.filter((video: Video) => video.status === "Pending");
+
+      const convertedVideos = pendingVideos.map((video: Video) => {
+        const startDownloadAtDate = new Date(video.start_download_at);
+        const downloadedAtDate = new Date(video.downloaded_at);
+        video.start_download_at = startDownloadAtDate.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
+        video.downloaded_at = downloadedAtDate.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
+        return video;
+      });
+
+      setVideos((prevVideos) => {
+        const updatedVideos = prevVideos.map((video) => {
+          const newVideo = convertedVideos.find((v: { id: string }) => v.id === video.id);
+          return newVideo || video;
+        });
+        return [
+          ...updatedVideos,
+          ...convertedVideos.filter((v: { id: string }) => !prevVideos.find((video) => v.id === video.id)),
+        ];
+      });
+      setIsLoading(false);
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 10000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
