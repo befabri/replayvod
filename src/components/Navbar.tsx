5d794765936aa4b2501041289a213f5536fc7426
Benjamin Fabri <contact@bfabri.me>
2023-06-20 16:31:00 +0200

    Update title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar: React.FC = () => {
               </button>
               <a href="/" className="flex ml-2 md:mr-24">
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  TwitchReplay
+                  Rediff
                 </span>
               </a>
             </div>
