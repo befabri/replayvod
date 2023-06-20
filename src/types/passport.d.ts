1c1253dc1652e8678aca9cef47e9877284054f3a
Benjamin Fabri <contact@bfabri.me>
2023-06-20 05:50:16 +0200

    Test CI/CD

--- src/types/passport.d.ts
+++ src/types/passport.d.ts
@@ -1,13 +1,14 @@
-export {};
+import session from "express-session";
+import express from "express";
 
 declare module "express-session" {
-  interface SessionData {
+  interface SessionData extends session.SessionData {
     passport: { [key: string]: any };
   }
 }
 
-declare module "express" {
-  export interface Request {
+declare module "express-serve-static-core" {
+  interface Request extends express.Request {
     session: any;
   }
 }
