f0560be5679b553a646f3baadeeb397b6563f94e
Benjamin Fabri <contact@bfabri.me>
2023-06-21 04:10:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 # entrypoint.sh
 
-addgroup -g $PGID appgroup
+# Check if the group exists and create it if it doesn't
+if ! getent group $PGID > /dev/null; then
+    addgroup -g $PGID appgroup
+fi
 
-adduser -D -u $PUID -s /bin/sh -G appgroup appuser
+# Check if the user exists and create it if it doesn't
+if ! getent passwd $PUID > /dev/null; then
+    adduser -D -u $PUID -s /bin/sh -G appgroup appuser
+else
+    addgroup $(getent passwd $PUID | cut -d: -f1) appgroup
+fi
 
-su - appuser
+# Change the ownership of the volume directories
+chown -R $PUID:$PGID /app/log /app/public
 
+# Switch to the new user
+su - $(getent passwd $PUID | cut -d: -f1)
+
+# Execute the command (CMD [ "node", "app.js" ])
 exec "$@"
