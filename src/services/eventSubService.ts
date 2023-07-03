28a08129d6f1d70da9aac07cd0c3ee909869741d
Benjamin Fabri <contact@bfabri.me>
2023-07-03 19:30:37 +0200

    Fix subToAllStreamEventFollowed

--- src/services/eventSubService.ts
+++ src/services/eventSubService.ts
@@ -17,15 +17,15 @@ class eventSubService {
 
     async subToAllStreamEventFollowed() {
         console.log("getting followed channels");
-        const followedChannels = await this.userService.getUserFollowedChannelsDb();
-        console.log(JSON.stringify(followedChannels));
+        const followedChannelsArr = await this.userService.getUserFollowedChannelsDb();
         let responses = [];
-        for (const channel of followedChannels) {
-            console.log(channel);
-            console.log(channel.broadcaster.id);
-            const respOnline = await this.subscribeToStreamOnline(channel.broadcaster.id);
-            const respOffline = await this.subscribeToStreamOffline(channel.broadcaster.id);
-            responses.push({ channel: channel.broadcaster.id, online: respOnline, offline: respOffline });
+        for (const followedChannels of followedChannelsArr) {
+            for (const channel of followedChannels.channels) {
+                console.log(channel);
+                const respOnline = await this.subscribeToStreamOnline(channel.broadcaster_id);
+                const respOffline = await this.subscribeToStreamOffline(channel.broadcaster_id);
+                responses.push({ channel: channel.broadcaster_id, online: respOnline, offline: respOffline });
+            }
         }
         for (const resp of responses) {
             webhookEventLogger.info(
