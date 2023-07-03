a3ac5f96fec30da09a41cd4d6dab8708469915ba
Benjamin Fabri <contact@bfabri.me>
2023-07-03 19:38:32 +0200

    Add debug log for createEventSub

--- src/utils/twitchAPI.ts
+++ src/utils/twitchAPI.ts
@@ -173,7 +173,7 @@ class TwitchAPI {
                     },
                 }
             );
-
+            console.log(response);
             return response.data;
         } catch (error) {
             throw error;
