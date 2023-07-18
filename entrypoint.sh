468446d6aae610345504f472751b48b440f5b441
Benjamin Fabri <recvfeeds@pm.me>
2023-07-18 12:47:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
 # entrypoint.sh
 
-# Update user 'node' to have the specified UID and GID
-if getent passwd $PUID > /dev/null; then
-    deluser node
-    adduser -u $PUID -D node
-else
-    adduser -u $PUID -D node
-fi
-
-if getent group $PGID > /dev/null; then
-    delgroup node
-    addgroup -g $PGID node
-else
-    addgroup -g $PGID node
-fi
+addgroup -g $PGID abc
+adduser -u $PUID -G abc -D abc
 
 
 # Change the ownership of the volume directories
-chown -R $PUID:$PGID /app/log /app/public /app/data /app/bin
+chown -R abc:abc /app/log /app/public /app/data /app/bin
 chmod +x bin/yt-dlp
 
 # Execute the command (CMD [ "node", "app.js" ])
