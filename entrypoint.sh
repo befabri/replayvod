5d69feb620c8c2aaffd70940efa8f37d95e64986
Benjamin Fabri <recvfeeds@pm.me>
2023-07-18 12:52:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 # entrypoint.sh
 
-addgroup -g $PGID abc
-adduser -u $PUID -G abc -D abc
-
-
 # Change the ownership of the volume directories
-chown -R abc:abc /app/log /app/public /app/data /app/bin
+chown -R $PUID:$PGID /app/log /app/public /app/data /app/bin
 chmod +x bin/yt-dlp
 
 # Execute the command (CMD [ "node", "app.js" ])
