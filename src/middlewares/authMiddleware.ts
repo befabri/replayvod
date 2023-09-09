6f02785f1735f1b34826a4b6d3e7ac2d6f9a8195
Benjamin Fabri <recvfeeds@pm.me>
2023-09-09 17:06:59 +0200

    Update middlewares

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,19 +6,19 @@ dotenv.config();
 const WHITELISTED_USER_IDS: string[] = process.env.WHITELISTED_USER_IDS?.split(",") || [];
 const IS_WHITELIST_ENABLED: boolean = process.env.IS_WHITELIST_ENABLED?.toLowerCase() === "true";
 
-export function isUserWhitelisted(req: Request, res: Response, next: NextFunction) {
-  const userID = req.session?.passport?.user?.twitchId;
-  if (!IS_WHITELIST_ENABLED || (userID && WHITELISTED_USER_IDS.includes(userID))) {
-    next();
-  } else {
-    res.status(403).json({ error: "Forbidden, you're not on the whitelist." });
-  }
+export async function isUserWhitelisted(req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) {
+    const userID = req.session?.passport?.user?.twitchId;
+    if (!IS_WHITELIST_ENABLED || (userID && WHITELISTED_USER_IDS.includes(userID))) {
+        done();
+    } else {
+        reply.status(403).send({ error: "Forbidden, you're not on the whitelist." });
+    }
 }
 
-export function userAuthenticated(req: Request, res: Response, next: NextFunction) {
-  if (req.session?.passport?.user) {
-    next();
-  } else {
-    res.status(401).json({ error: "Unauthorized" });
-  }
+export async function userAuthenticated(req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) {
+    if (req.session?.passport?.user) {
+        done();
+    } else {
+        reply.status(401).send({ error: "Unauthorized" });
+    }
 }
--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,9 +1,11 @@
-import passport from "passport";
-import { Strategy as OAuth2Strategy } from "passport-oauth2";
+// passport.ts
+import fastifyPassport from "@fastify/passport";
 import axios from "axios";
 import dotenv from "dotenv";
+import { Strategy as OAuth2Strategy } from "passport-oauth2";
 
 dotenv.config();
+
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 const TWITCH_SECRET = process.env.TWITCH_SECRET;
 const CALLBACK_URL = process.env.CALLBACK_URL;
@@ -11,56 +13,58 @@ const WHITELISTED_USER_IDS: string[] = process.env.WHITELISTED_USER_IDS?.split("
 const IS_WHITELIST_ENABLED: boolean = process.env.IS_WHITELIST_ENABLED?.toLowerCase() === "true";
 
 OAuth2Strategy.prototype.userProfile = function (accessToken: string, done: Function) {
-  axios({
-    url: "https://api.twitch.tv/helix/users",
-    method: "GET",
-    headers: {
-      "Client-ID": TWITCH_CLIENT_ID,
-      Accept: "application/vnd.twitchtv.v5+json",
-      Authorization: "Bearer " + accessToken,
-    },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        done(null, response.data);
-      } else {
-        done(response.data);
-      }
+    axios({
+        url: "https://api.twitch.tv/helix/users",
+        method: "GET",
+        headers: {
+            "Client-ID": TWITCH_CLIENT_ID,
+            Accept: "application/vnd.twitchtv.v5+json",
+            Authorization: "Bearer " + accessToken,
+        },
     })
-    .catch((err) => {
-      done(err);
-    });
+        .then((response) => {
+            if (response.status === 200) {
+                done(null, response.data);
+            } else {
+                done(response.data);
+            }
+        })
+        .catch((err) => {
+            done(err);
+        });
 };
 
-passport.serializeUser((user: any, done: any) => {
-  done(null, user);
+fastifyPassport.registerUserSerializer(async (user: any, request) => {
+    return user;
 });
 
-passport.deserializeUser((user: any, done: any) => {
-  done(null, user);
+fastifyPassport.registerUserDeserializer(async (user: any, request) => {
+    return user;
 });
 
-passport.use(
-  "twitch",
-  new OAuth2Strategy(
-    {
-      authorizationURL: "https://id.twitch.tv/oauth2/authorize",
-      tokenURL: "https://id.twitch.tv/oauth2/token",
-      clientID: TWITCH_CLIENT_ID || "",
-      clientSecret: TWITCH_SECRET || "",
-      callbackURL: CALLBACK_URL || "",
-      state: true,
-    },
-    (accessToken: string, refreshToken: string, profile: any, done: any) => {
-      profile.accessToken = accessToken;
-      profile.refreshToken = refreshToken;
-      profile.twitchId = profile.data[0].id;
+fastifyPassport.use(
+    "twitch",
+    new OAuth2Strategy(
+        {
+            authorizationURL: "https://id.twitch.tv/oauth2/authorize",
+            tokenURL: "https://id.twitch.tv/oauth2/token",
+            clientID: TWITCH_CLIENT_ID || "",
+            clientSecret: TWITCH_SECRET || "",
+            callbackURL: CALLBACK_URL || "",
+            state: true,
+        },
+        (accessToken: string, refreshToken: string, profile: any, done: any) => {
+            profile.accessToken = accessToken;
+            profile.refreshToken = refreshToken;
+            profile.twitchId = profile.data[0].id;
 
-      if (!IS_WHITELIST_ENABLED || WHITELISTED_USER_IDS.includes(profile.twitchId)) {
-        done(null, profile);
-      } else {
-        done(null, null);
-      }
-    }
-  )
+            if (!IS_WHITELIST_ENABLED || WHITELISTED_USER_IDS.includes(profile.twitchId)) {
+                done(null, profile);
+            } else {
+                done(null, null);
+            }
+        }
+    )
 );
+
+export default fastifyPassport;
