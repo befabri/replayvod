06565656302b52f3abab6768d9a76bff49c83a77
Benjamin Fabri <contact@bfabri.me>
2023-09-12 22:27:03 +0200

    Update AppAccessToken

--- src/utils/twitchUtils.ts
+++ src/utils/twitchUtils.ts
@@ -1,41 +1,43 @@
 import axios from "axios";
-import { getDbInstance } from "../models/db";
+import { logger as rootLogger } from "../app";
+import { prisma } from "../server";
+const logger = rootLogger.child({ service: "accessToken" });
 
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 const TWITCH_SECRET = process.env.TWITCH_SECRET;
 
 export const getAppAccessToken = async () => {
-  const db = await getDbInstance();
-  const collection = db.collection("appAccessToken");
-
-  const currentTimestamp = Date.now();
-  const tokenDocument = await collection.findOne({ expiresAt: { $gt: currentTimestamp } });
-
-  if (tokenDocument) {
-    return tokenDocument.accessToken;
-  }
-
-  try {
-    const response = await axios.post("https://id.twitch.tv/oauth2/token", null, {
-      params: {
-        client_id: TWITCH_CLIENT_ID,
-        client_secret: TWITCH_SECRET,
-        grant_type: "client_credentials",
-      },
+    logger.info("Access token...");
+    const latestToken = await prisma.appAccessToken.findFirst({
+        orderBy: {
+            expiresAt: "desc",
+        },
     });
-
-    const newToken = response.data.access_token;
-    const tokenLifetime = response.data.expires_in * 1000; // Convert to milliseconds
-
-    const newTokenDocument = {
-      accessToken: newToken,
-      expiresAt: currentTimestamp + tokenLifetime,
-    };
-    await collection.insertOne(newTokenDocument);
-
-    return newToken;
-  } catch (error) {
-    console.error("Error getting app access token:", error);
-    throw error;
-  }
+    if (latestToken) {
+        return latestToken.accessToken;
+    }
+    try {
+        logger.info("Fetching a new access token...");
+        const response = await axios.post("https://id.twitch.tv/oauth2/token", null, {
+            params: {
+                client_id: TWITCH_CLIENT_ID,
+                client_secret: TWITCH_SECRET,
+                grant_type: "client_credentials",
+            },
+        });
+        const newToken = response.data.access_token;
+        const tokenLifetime = response.data.expires_in * 1000;
+        const currentTimestamp = new Date();
+        const expiresAt = new Date(currentTimestamp.getTime() + tokenLifetime);
+        await prisma.appAccessToken.create({
+            data: {
+                accessToken: newToken,
+                expiresAt: expiresAt,
+            },
+        });
+        return newToken;
+    } catch (error) {
+        logger.error("Error getting app access token:", error);
+        throw error;
+    }
 };
