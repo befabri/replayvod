1f44632c53949d46e3e620963134fbdbd93616fe
Benjamin Fabri <contact@bfabri.me>
2023-10-01 00:09:52 +0200

    Add new components

--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -1,27 +1,27 @@
-import { FC, ChangeEvent } from "react";
+import { FC } from "react";
 
 interface InputNumberProps {
-  label: string;
-  id: string;
-  value: number;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  required?: boolean;
+    id: string;
+    required?: boolean;
+    register?: any;
+    error?: any;
+    disabled?: boolean;
+    minValue: number;
 }
 
-const InputNumber: FC<InputNumberProps> = ({ label, id, value, onChange, required }) => (
-  <div>
-    <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-      {label}
-    </label>
-    <input
-      type="number"
-      id={id}
-      value={value}
-      onChange={onChange}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      required={required}
-    />
-  </div>
+const InputNumber: FC<InputNumberProps> = ({ id, required, register, error, disabled, minValue }) => (
+    <>
+        <input
+            {...register}
+            type="number"
+            id={id}
+            required={required}
+            min={minValue}
+            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+            ${disabled ? "dark:bg-gray-800" : "dark:bg-gray-700"}`}
+        />
+        {error && <p className=" text-red-500 italic px-2 py-1 rounded-md self-start">{error?.message}</p>}
+    </>
 );
 
 export default InputNumber;
