5567e31a9fcb7bd5d1101ad826129f0a46dd896e
Benjamin Fabri <contact@bfabri.me>
2023-10-08 19:33:23 +0200

    Improve video info component css

--- src/components/Media/VideoInfo.tsx
+++ src/components/Media/VideoInfo.tsx
@@ -31,25 +31,33 @@ const VideoInfoComponent: FC<VideoInfoProps> = ({ video, disablePicture = false 
     }, []);
 
     return (
-        <div className="flex flex-row items-center gap-3 mt-2" ref={divRef}>
+        <div className="flex flex-row items-center gap-5 mt-2" ref={divRef}>
             {!disablePicture && (
                 <a href={`${Pathnames.Channel}${video?.displayName.toLowerCase()}`}>
                     <img
-                        className="w-12 h-12 min-w-[10px] min-h-[10px] rounded-full ml-2"
+                        className="w-12 h-12 min-w-[40px] rounded-full ml-2"
                         src={video?.channel.profilePicture}
                         alt="Profile Picture"
                     />
                 </a>
             )}
             <div>
-                <h3 className="text-base font-medium dark:text-stone-100">{video?.titles[0].title.name}</h3>
+                <a href={`${Pathnames.Watch}${video?.id}`}>
+                    <h3 className="text-base font-semibold dark:text-stone-100 hover:text-sky-500 dark:hover:text-sky-500">
+                        {video?.titles[0].title.name}
+                    </h3>
+                </a>
                 <a href={`${Pathnames.Channel}${video?.displayName.toLowerCase()}`}>
-                    <h3 className="text-base  dark:text-stone-100">{video?.channel.broadcasterName}</h3>
+                    <h2 className="text-sm dark:text-stone-100 hover:text-sky-500 dark:hover:text-sky-500">
+                        {video?.channel.broadcasterName}
+                    </h2>
                 </a>
 
                 {video?.videoCategory.map((item) => (
                     <a href={`${Pathnames.Vod}/${toKebabCase(item.category.name)}`} key={item.categoryId}>
-                        <p className="text-base  dark:text-violet-400">{item.category.name}</p>
+                        <p className="text-sm dark:text-violet-400 hover:text-violet-500 dark:hover:text-violet-500">
+                            {item.category.name}
+                        </p>
                     </a>
                 ))}
                 {video?.tags.slice(0, numberOfTagsToRender).map((item) => (
