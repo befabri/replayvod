8ccf17f3c7a89dae58b6fffeaef7c244c4566f86
Benjamin Fabri <contact@bfabri.me>
2023-10-08 19:19:20 +0200

    Fix whitelist using a function instead of accessing it directly

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import dotenv from "dotenv";
 import { logger as rootLogger } from "../app";
+import { userService } from "../api/user";
 const logger = rootLogger.child({ domain: "auth", service: "authMiddleware" });
 
 dotenv.config();
@@ -9,8 +10,8 @@ const WHITELISTED_USER_IDS: string[] = process.env.WHITELISTED_USER_IDS?.split("
 const IS_WHITELIST_ENABLED: boolean = process.env.IS_WHITELIST_ENABLED?.toLowerCase() === "true";
 
 export const isUserWhitelisted = async (req: FastifyRequest, reply: FastifyReply) => {
-    const userID = req.session?.user?.twitchId;
-    if (IS_WHITELIST_ENABLED && (!userID || !WHITELISTED_USER_IDS.includes(userID))) {
+    const userId = userService.getUserIdFromSession(req);
+    if (IS_WHITELIST_ENABLED && (!userId || !WHITELISTED_USER_IDS.includes(userId))) {
         logger.error(`Forbidden, you're not on the whitelist.`);
         reply.status(403).send({ error: "Forbidden, you're not on the whitelist." });
         throw new Error("Not on the whitelist");
