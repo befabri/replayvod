481ec2cda97dd909cd01d0bb55ee12983aea1513
Benjamin Fabri <contact@bfabri.me>
2023-10-26 10:31:08 +0200

    Fix webhook auth

--- src/api/webhook/webhook.routes.ts
+++ src/api/webhook/webhook.routes.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance } from "fastify";
 import { webhookHandler } from ".";
+import { isUserWhitelisted, userAuthenticated } from "../../middlewares/authMiddleware";
 
 export default function (fastify: FastifyInstance, opts: any, done: any) {
-    fastify.post("/webhooks", webhookHandler.addWebhook);
-    fastify.delete("/webhooks", webhookHandler.removeWebhook);
-    fastify.post("/webhooks/callback", webhookHandler.callbackWebhook);
+    // fastify.get("/webhooks/test", webhookHandler.test);
+
+    fastify.post("/webhooks", {
+        preHandler: [isUserWhitelisted, userAuthenticated],
+        handler: webhookHandler.addWebhook,
+    });
+
+    fastify.delete("/webhooks", {
+        preHandler: [isUserWhitelisted, userAuthenticated],
+        handler: webhookHandler.removeWebhook,
+    });
+
+    fastify.post("/webhooks/callback", {
+        handler: webhookHandler.callbackWebhook,
+    });
 
     done();
 }
