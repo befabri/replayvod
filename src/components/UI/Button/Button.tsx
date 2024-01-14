dac668fb9000d57b0b0d58b97a79b38739b2ea67
Benjamin Fabri <bfabri@pm.me>
2024-01-14 03:36:58 +0100

    Add svg button style

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -5,19 +5,20 @@ export interface ButtonProps {
     onClick?: MouseEventHandler<HTMLButtonElement>;
     typeButton?: "button" | "submit" | "reset";
     disabled?: boolean;
-    style?: "primary" | "inverted" | "submit";
+    style?: "primary" | "inverted" | "submit" | "svg";
     children?: React.ReactNode;
 }
 
 const styles = {
     primary:
-        " flex items-center px-6 py-2 text-white bg-custom_lightblue rounded-md hover:bg-custom_vista_blue font-medium ",
+        " flex items-center px-6 py-2 text-white bg-custom_lightblue rounded-md hover:bg-custom_vista_blue font-medium",
     inverted:
-        " flex items-center px-6 py-2 text-white bg-custom_vista_blue rounded-md hover hover:bg-custom_lightblue font-medium ",
-    submit: " flex items-center px-6 py-2 text-white bg-custom_delft_blue rounded-md hover:bg-custom_vista_blue font-medium ",
+        " flex items-center px-6 py-2 text-white bg-custom_vista_blue rounded-md hover hover:bg-custom_lightblue font-medium",
+    submit: " flex items-center px-6 py-2 text-white bg-custom_delft_blue rounded-md hover:bg-custom_vista_blue font-medium",
+    svg: " flex items-center focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2.5  dark:text-white  dark:hover:bg-custom_vista_blue dark:focus:ring-gray-700",
 };
 
-const getButtonStyle = (styleType: "primary" | "inverted" | "submit") => {
+const getButtonStyle = (styleType: "primary" | "inverted" | "submit" | "svg") => {
     return styles[styleType] || styles.primary;
 };
 const Button: FC<ButtonProps> = ({
