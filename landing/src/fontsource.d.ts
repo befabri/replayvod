aebc1a200efdd82899091eacae5ae1eee2f61007
Benjamin Fabri <bfabri@pm.me>
2026-06-06 02:43:22 +0200

    chore(landing): update dependencies

---
+++ landing/src/fontsource.d.ts
@@ -0,0 +1,4 @@
+declare module "@fontsource-variable/figtree";
+declare module "@fontsource-variable/figtree/wght-italic.css";
+declare module "@fontsource-variable/outfit";
+declare module "@fontsource-variable/jetbrains-mono";
